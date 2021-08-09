d51e44688a4af980efe73bbfecf126007f31dd21
dangcapqaz@gmail.com <dangcapqaz@gmail.com>
2021-08-09 17:58:29 +0700

    update UI

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,16 +14,29 @@ $(document).ready(function () {
       },
     ],
   });
+
+  let headerMenu = document.querySelector(".header-menu");
+  let toggleMenu = document.querySelector(".header-toggle");
+  let classExpand = "is-expand";
+  toggleMenu.addEventListener("click", () => {
+    headerMenu.classList.add(classExpand);
+  });
+  window.addEventListener("click", (e) => {
+    if (!headerMenu.contains(e.target) && !e.target.classList.contains("header-toggle")) {
+      headerMenu.classList.remove(classExpand);
+    }
+  })
+
+  let wedoItemHeading = $(".wedo-item-heading");
+  let wedoItem= $(".wedo-item");
+  wedoItemHeading.click(function (e) {
+    if ($(this).parent().hasClass("is-active")) {
+      $(this).parent().removeClass("is-active");
+    }
+    else {
+      wedoItem.removeClass("is-active");
+      $(this).parent().addClass("is-active");
+    }
+  }) 
 });
 
-let headerMenu = document.querySelector(".header-menu");
-let toggleMenu = document.querySelector(".header-toggle");
-let classExpand = "is-expand";
-toggleMenu.addEventListener("click", () => {
-  headerMenu.classList.add(classExpand);
-});
-window.addEventListener("click", (e) => {
-  if (!headerMenu.contains(e.target) && !e.target.classList.contains("header-toggle")) {
-    headerMenu.classList.remove(classExpand);
-  }
-})
